e05a94ca245739a390f07da91195407802a5f69c
Hakan Nisgavlioğlu <hakannisgav@gmail.com>
2024-09-20 21:41:57 +0300

    Integrate task services into Program.cs for task management
    - Integrated TaskService and ITaskService into Program.cs to handle all task operations through service methods.
    - Updated command handling to use service methods for adding, updating, deleting, and listing tasks.
    - Improved error handling and user feedback by leveraging service logic.
    - Refactored Program.cs for cleaner and more maintainable code structure, utilizing task services for better separation of concerns.

--- TaskCli/Program.cs
+++ TaskCli/Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using TaskCli.Services;
+using Task = TaskCli.Models.Task;
+
+var tasks = new List<Task>();
+var taskManager = new TaskService(tasks);
+
+static void ShowUsage()
+{
+    Console.WriteLine("Usage:");
+    Console.WriteLine(" task-cli add \"Task description\"");
+    Console.WriteLine(" task-cli update <id, description, status> \"New description and new status\"");
+    Console.WriteLine(" task-cli delete <id>");
+    Console.WriteLine(" task-cli list");
+    Console.WriteLine(" task-cli list-by-status <status> \"Get By Status\"");
+}
+
+if (args.Length == 0)
+{
+    ShowUsage();
+    return;
+}
+
+try
+{
+    var command = args[0].ToLower();
+
+    switch (command)
+    {
+        case "add":
+            if (!(args.Length < 2)) taskManager.AddTask(args[1]);
+            break;
+        case "update":
+            if (!(args.Length < 4)) taskManager.UpdateTask(Guid.Parse(args[1]), args[2], args[3]);
+            break;
+        case "delete":
+            if (!(args.Length < 2)) taskManager.DeleteTask(Guid.Parse(args[1]));
+            break;
+        case "list":
+            taskManager.GetAllTask();
+            break;
+        case "list-by-status":
+            string status = args.Length > 1 ? args[1].ToLower() : null;
+            taskManager.GetAllTaskByStatus(status);
+            break;
+        default:
+            ShowUsage();
+            break;
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+    ShowUsage();
+}
