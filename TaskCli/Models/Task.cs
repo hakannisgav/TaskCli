8a1ff78c058979eded6780efcc4e78a580c97ba3
Hakan Nisgavlioğlu <hakannisgav@gmail.com>
2024-09-20 21:38:53 +0300

    -Task model created, -ITaskService created, -TaskService Function created

---
+++ TaskCli/Models/Task.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace TaskCli.Models;
+
+public class Task
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public string Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+
+    public Task(string description)
+    {
+        Description = description;
+    }
+    
+    public Task(Guid id, string description, string status, DateTime createdAt, [Optional] DateTime updatedAt)
+    {
+        Id = id;
+        Description = description;
+        Status = status;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+}
